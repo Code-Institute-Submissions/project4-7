263d71126327cc0336a3e32775e021719cccf47c
Donovan Lo <donovanlo1174@gmail.com>
2020-10-18 07:47:57 +0000

    Route Auth to local templates

--- EcommerceWeb/settings.py
+++ EcommerceWeb/settings.py
@@ -69,7 +69,10 @@ ROOT_URLCONF = 'EcommerceWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
