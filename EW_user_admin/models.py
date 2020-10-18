90e9ddbe4a72f951b3265c0822a985e0fedebada
Donovan Lo <donovanlo1174@gmail.com>
2020-10-18 01:43:27 +0000

    Create v1 model for products, add to admin.py

--- EW_user_admin/models.py
+++ EW_user_admin/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    
+    # NAME OF PRODUCT
+    name = models.CharField(
+        max_length=8,
+        blank=False
+    )
+
+    # CATEGORY THE PRODUCT BELONG TO
+    CATEGORY = [
+        ('Formal-Wear','Formal-Wear'),
+        ('Casual-Wear','Casual-Wear'),
+        ('Accessories','Accessories'),
+        ('Ties','Ties'),
+    ]
+    category = models.CharField(
+        max_length=15,
+        choices=CATEGORY,
+        default='Formal-Wear',
+        blank=False
+    )
+
+    # ORIGINAL PRICING OF PRODUCT TO BE SOLD
+    price = models.DecimalField(
+        blank=False,
+        max_digits=5,
+        decimal_places=2
+    )
+                                        
+    # DESCRIPTION FOR THE PRODUCT
+    description = models.TextField(
+        max_length = 255
+    )
+    # CURRENT STOCK QUANTITY
+    stock_qty = models.IntegerField(
+        blank = False
+    )
+    
+
+    def __str__(self):
+        return (self.name)
+    
