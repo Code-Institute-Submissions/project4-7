b820ff02792ae55d4577a45d402457603b2dcaa9
Donovan Lo <donovanlo1174@gmail.com>
2020-10-18 02:56:14 +0000

    pass form details of selected product for editing

--- EW_user_admin/urls.py
+++ EW_user_admin/urls.py
@@ -3,5 +3,5 @@ import EW_user_admin.views
 
 urlpatterns = [
     path('', EW_user_admin.views.user_info, name='user_info'),
-    path('edit/', EW_user_admin.views.edit_info, name='edit_info'),
+    path('edit/<product_id>', EW_user_admin.views.edit_info, name='edit_info'),
 ]
--- EW_user_admin/views.py
+++ EW_user_admin/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def user_info (request):
@@ -8,5 +9,9 @@ def user_info (request):
         'products' : products
     })
 
-def edit_info (request):
-    return render (request, 'edit_info.template.html')
+def edit_info (request, product_id):
+    product_to_be_edit = get_object_or_404(Product, pk=product_id)
+    edit_form = ProductForm(instance=product_to_be_edit)
+    return render (request, 'edit_info.template.html', {
+        'form' : edit_form
+    })
