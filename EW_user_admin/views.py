211e25acf05616a1bd78114009d5fa4bec3b3a16
Donovan Lo <donovanlo1174@gmail.com>
2020-10-18 02:30:33 +0000

    display database products in html page

--- EW_user_admin/views.py
+++ EW_user_admin/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from .models import Product
 
 # Create your views here.
 def user_info (request):
-    return render (request, 'user_info.template.html')
+    products = Product.objects.all()
+    return render (request, 'user_info.template.html', {
+        'products' : products
+    })
