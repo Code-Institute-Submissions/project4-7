dbfb7e8221b6bef1ddca8febe00f481ed394ff46
Donovan Lo <donovanlo1174@gmail.com>
2020-10-18 06:18:01 +0000

    Make function name consistent, added remove route

--- EW_user_admin/urls.py
+++ EW_user_admin/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 import EW_user_admin.views
 
 urlpatterns = [
-    path('', EW_user_admin.views.user_info, name='user_info'),
-    path('edit/<product_id>', EW_user_admin.views.edit_info, name='edit_info'),
+    path('', EW_user_admin.views.product_info, name='product_info'),
+    path('edit/<product_id>', EW_user_admin.views.edit_product, name='edit_product'),
     path('add', EW_user_admin.views.add_product, name='add_product'),
+    path('remove/<product_id>', EW_user_admin.views.remove_product, name='remove_product'),
 ]
--- EW_user_admin/views.py
+++ EW_user_admin/views.py
@@ -3,21 +3,21 @@ from .models import Product
 from .forms import ProductForm
 
 # Create your views here.
-def user_info (request):
+def product_info (request):
     products = Product.objects.all()
-    return render (request, 'user_info.template.html', {
+    return render (request, 'product_info.template.html', {
         'products' : products
     })
 
-def edit_info (request, product_id):
+def edit_product (request, product_id):
     product_to_be_edit = get_object_or_404(Product, pk=product_id)
     if request.method == "POST":
         edit_form = ProductForm(request.POST, instance=product_to_be_edit)
         edit_form.save()
-        return redirect(reverse(user_info))
+        return redirect(reverse(product_info))
     else:
         edit_form = ProductForm(instance=product_to_be_edit)
-        return render (request, 'edit_info.template.html', {
+        return render (request, 'edit_product.template.html', {
             'form' : edit_form
         })
 
@@ -25,9 +25,12 @@ def add_product (request):
     if request.method == "POST":
         form = ProductForm(request.POST)
         form.save()
-        return redirect(reverse(user_info))
+        return redirect(reverse(product_info))
     else:
         form = ProductForm()
         return render (request, 'add_product.template.html', {
             'form' : form
         })
+
+def remove_product(request, product_id):
+    return render(request,'remove_product.template.html')
