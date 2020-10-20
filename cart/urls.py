a7bdc9c88a511f9f2af99f95a69ac620ef38b439
Donovan Lo <donovanlo1174@gmail.com>
2020-10-20 20:41:34 +0000

    create and link add_cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,5 @@ import cart.views
 
 urlpatterns = [
     path('summary/', cart.views.view_cart, name='view_cart'),
+    path('add/<product_id>', cart.views.add_cart, name='add_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def view_cart(request):
     return render (request, 'view_cart.template.html')
+
+def add_cart(request, product_id):
+    return render (request, 'add_cart.template.html')
