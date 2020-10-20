e9eea09bdfd9616d51c91838e18e49c233f517d6
Donovan Lo <donovanlo1174@gmail.com>
2020-10-20 21:46:53 +0000

    pass cart info to summary.html and display info

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from user_console.models import Product
+from django.contrib import messages
 
 # Create your views here.
 def view_cart(request):
-    return render (request, 'view_cart.template.html')
+    cart = request.session.get('shopping_cart', {})
+    return render (request, 'view_cart.template.html', {
+        'cart': cart,
+    })
 
 def add_cart(request, product_id):
-    return render (request, 'add_cart.template.html')
+    # load cart detail
+    cart = request.session.get('shopping_cart', {})
+    # check whether the product_is not in the cart. else we will add it
+    if product_id not in cart:
+        product = get_object_or_404(Product, pk=product_id)
+        # product is found, let's add it to the cart
+
+        cart[product_id] = {
+            'id': product_id,
+            'name': product.name,
+            'price': float(product.price),
+            'qty' : 1,
+        }
+
+        # save the cart back to sessions
+        request.session['shopping_cart'] = cart
+        messages.success(request, "product has been added to your cart!")
+        # return redirect(reverse('view_cart'))
+    else:
+        cart[product_id]['qty'] +=1        
+        # save the cart back to sessions
+        request.session['shopping_cart'] = cart
+
+        # return HttpResponse('Product added')
+    return redirect(reverse('view_cart'))
