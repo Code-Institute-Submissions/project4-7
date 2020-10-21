b3dde16bda7744e46c0d5124b01cd128ee839849
Donovan Lo <donovanlo1174@gmail.com>
2020-10-21 03:56:39 +0000

    added add function route to add quantity to selected product

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('summary/', cart.views.view_cart, name='view_cart'),
     path('add/<product_id>', cart.views.add_cart, name='add_cart'),
     path('remove/<product_id>', cart.views.remove_item, name='remove_item'),
+    path('add_qty/<product_id>', cart.views.add_qty, name='add_qty'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from user_console.models import Product
 from django.contrib import messages
+from main_app.views import shop
 
 # Create your views here.
 def view_cart(request):
@@ -21,18 +22,18 @@ def add_cart(request, product_id):
             'id': product_id,
             'name': product.name,
             'price': float(product.price),
-            'qty' : 1,
+            'qty' : 1, 
         }
 
         # save the cart back to sessions
         request.session['shopping_cart'] = cart
         messages.success(request, "product has been added to your cart!")
+        return redirect(reverse('shop'))
     else:
         cart[product_id]['qty'] +=1        
         # save the cart back to sessions
         request.session['shopping_cart'] = cart
-
-    return redirect(reverse('view_cart'))
+        return redirect(reverse('view_cart'))
 
 def remove_item (request, product_id):
     cart = request.session.get('shopping_cart', {})
@@ -41,4 +42,10 @@ def remove_item (request, product_id):
         del cart[product_id]
         request.session['shopping_cart'] = cart
 
+    return redirect(reverse('view_cart'))
+
+def add_qty (request, product_id):
+    cart = request.session.get('shopping_cart', {})
+    cart[product_id]['qty'] += 1
+    request.session['shopping_cart'] = cart
     return redirect(reverse('view_cart'))
