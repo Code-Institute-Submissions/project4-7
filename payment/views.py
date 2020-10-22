868737742b20cc459b47f384c49ec68528734611
Donovan Lo <donovanlo1174@gmail.com>
2020-10-22 03:35:22 +0000

    update stock quantity once payment made

--- payment/views.py
+++ payment/views.py
@@ -3,10 +3,13 @@ from django.conf import settings
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from main_app.views import index
 from payment.models import Payment_log
+from user_console.models import Product
+from user_console.forms import ProductForm
 
 # Create your views here.
 def checkout(request):
     cart = request.session.get("shopping_cart", {})
+
     amount_to_charge = 100
     if request.method == 'GET':
         amount = amount_to_charge 
@@ -16,7 +19,6 @@ def checkout(request):
             'amount' : amount,
             'cart' : cart,
         })
-        # Update Log
     else:
         stripe.api_key = settings.STRIPE_SECRET_KEY 
         charge = stripe.Charge.create(
@@ -27,7 +29,16 @@ def checkout(request):
         )
 
         # Clear cart
+        request.session['shopping_cart'] = {}
 
+        # Update Product Remaining Quantity
+        for item_id in cart:
+            product_to_update = get_object_or_404(Product, pk=item_id)
+            print(cart[item_id]['qty'])
+        
+            product_to_update.stock_qty = product_to_update.stock_qty - cart[item_id]['qty']
+            product_to_update.save()
         # Update Log
+        
             
         return redirect(reverse('index'))
